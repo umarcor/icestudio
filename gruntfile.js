73c5db4add60c65fbafa4b01eff207be64d2ca17
umarcor <unai.martinezcorral@ehu.eus>
2019-12-26 21:21:45 +0100

    gruntfile: remove redundant task 'gettext'

--- gruntfile.js
+++ gruntfile.js
@@ -416,9 +416,6 @@
   grunt.registerTask('default', function() {
     console.log('Icestudio');
   });
-  grunt.registerTask('gettext', [
-    'nggettext_extract'
-  ]);
   grunt.registerTask('getcollection', [
     'clean:collection',
     'wget:collection',
