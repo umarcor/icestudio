2cfd195bfd331df5619f9d3c7a51add544b67ab5
umarcor <unai.martinezcorral@ehu.eus>
2020-06-16 19:23:25 +0200

    docs: update

---
+++ docs/board.rst
@@ -0,0 +1,29 @@
+Board
+=====
+
+A dropdown menu on the top right corner allows to view/modify settings related to the target board:
+
++--------------------+------------------------------------------------------+
+|   Action           |             Description                              |
++====================+======================================================+
+| *Board name*       | Show the board SVG pinout                            |
++--------------------+------------------------------------------------------+
+| Constraints        | Show the board constraints configuration file        |
++--------------------+------------------------------------------------------+
+| Datasheet          | Open board/device datasheet in the sytem web browser |
++--------------------+------------------------------------------------------+
+| View board rules   | Show rules of the board                              |
++--------------------+------------------------------------------------------+
+| Toggle board rules | Enable/disable rules of the board                    |
++--------------------+------------------------------------------------------+
+| Change board       | Select a different board                             |
++--------------------+------------------------------------------------------+
+
+Board rules (see :ref:`rules`) allow to automate tasks such as default port connections or default pin values. For example:
+
+* Connectint all unconnected *clk* ports to the internal CLK signal.
+* Turn all unused LEDs off.
+
+When a board is selected, all I/O blocks are updated and the values are reset.
+
+.. hint:: The configuration sources of board are stored in ``app/resources/boards``. In order to support a new board, create a new directory with the ``info.json``, ``pinout.*`` and ``pinout.*`` (optional) files. ``pinout.json`` MUST be generated from the ``pinout.*`` using the ``generator.py`` script.
--- docs/conf.py
+++ docs/conf.py
@@ -2,82 +2,60 @@
 
 extensions = []
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['source/_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
-# The master toctree document.
 master_doc = 'index'
 
-# General information about the project.
 project = u'Icestudio'
-copyright = u'2016-2019, Icestudio contributors'
+copyright = u'2016-2020, Icestudio contributors'
 author = u'Icestudio contributors'
 
-version = u'0.5.0' # The short X.Y version.
-release = u'0.5.0' # The full version, including alpha/beta/rc tags.
+title = u'Icestudio v1 (nightly)'
+
+version = u'0.5.1' # The short X.Y version.
+release = u'0.5.1dev' # The full version, including alpha/beta/rc tags.
 
 language = 'None'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['source/_static']
 
-# Output file base name for HTML help builder.
 htmlhelp_basename = 'icestudiodoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
 
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'icestudio.tex', u'icestudio Documentation',
-     u'Icestudio contributors', 'manual'),
+    (master_doc, 'icestudio.tex', title,
+     author, 'manual'),
 ]
 
 
 # -- Options for manual page output ---------------------------------------
 
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'icestudio', u'icestudio Documentation',
+    (master_doc, 'icestudio', title,
      [author], 1)
 ]
 
 
 # -- Options for Texinfo output -------------------------------------------
 
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
+
 texinfo_documents = [
-    (master_doc, 'icestudio', u'icestudio Documentation',
+    (master_doc, 'icestudio', title,
      author, 'icestudio', 'Visual editor for Verilog designs.',
      'Miscellaneous'),
 ]
